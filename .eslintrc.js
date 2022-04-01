2cede590dc4481445ca9a0e847bf0fb91225cd5a
Zach <zmriel@gmail.com>
2022-04-01 10:24:38 -0400

    update slate and plate

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,27 +6,34 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "plugin:react-hooks/recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:storybook/recommended",
     "plugin:jest-dom/recommended",
   ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 13,
-    sourceType: "module",
-  },
-  rules: {
-    "react-hooks/exhaustive-deps": [
-      "warn",
-      {
-        additionalHooks: "(useEventCallback)",
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 13,
+        sourceType: "module",
       },
-    ],
-  },
+      extends: [
+        "plugin:react/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended",
+        "plugin:@typescript-eslint/recommended",
+      ],
+      rules: {
+        "react-hooks/exhaustive-deps": [
+          "warn",
+          {
+            additionalHooks: "(useEventCallback)",
+          },
+        ],
+      },
+    },
+  ],
 };
