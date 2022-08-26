c3779989d5bdc666d1dcffadd748324ba3633899
Zach <zmriel@gmail.com>
2022-08-25 23:28:39 -0400

    build: use env var to enable bundle visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,48 +3,60 @@
 import typescript from "@rollup/plugin-typescript";
 import react from "@vitejs/plugin-react";
 import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  plugins: [
-    typescript({
-      exclude: [
-        "**/*.{spec,test}.{ts,tsx}",
-        "**/{spec,test,__test__}/*.{ts,tsx}",
-      ],
-    }),
-    react({
-      jsxImportSource: "@emotion/react",
-      babel: {
-        plugins: ["@emotion/babel-plugin"],
-      },
-    }),
-  ],
-  build: {
-    lib: {
-      entry: "./src/index.ts",
-      formats: ["es", "cjs"],
-      fileName: "index",
-    },
-    rollupOptions: {
-      external: [/node_modules/],
-      plugins: [
+export interface ViteConfigEnv {
+  ENABLE_BUNDLE_VISUALIZER?: boolean;
+}
+
+export default defineConfig(({ mode }) => {
+  const env: ViteConfigEnv = loadEnv(mode, process.cwd(), "");
+
+  const rollupPlugins = env.ENABLE_BUNDLE_VISUALIZER
+    ? [
         visualizer({
           sourcemap: true,
           template: "treemap", // sunburst | treemap | network
           filename: "./bundle-size/bundle.html",
         }),
-      ],
+      ]
+    : [];
+
+  return {
+    plugins: [
+      typescript({
+        exclude: [
+          "**/*.{spec,test}.{ts,tsx}",
+          "**/{spec,test,__test__}/*.{ts,tsx}",
+        ],
+      }),
+      react({
+        jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+      }),
+    ],
+    build: {
+      lib: {
+        entry: "./src/index.ts",
+        formats: ["es", "cjs"],
+        fileName: "index",
+      },
+      rollupOptions: {
+        external: [/node_modules/],
+        plugins: rollupPlugins,
+      },
+      sourcemap: true,
+      // Reduce bloat from legacy polyfills.
+      target: "esnext",
+      // Leave minification up to applications.
+      minify: false,
     },
-    sourcemap: true,
-    // Reduce bloat from legacy polyfills.
-    target: "esnext",
-    // Leave minification up to applications.
-    minify: false,
-  },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "./src/test/setup.ts",
-  },
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/test/setup.ts",
+    },
+  };
 });
